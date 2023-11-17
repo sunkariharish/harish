0b7d566888c6bd589d16ee35ccf0bf6a3f1c49d3
Admin <Admin@DESKTOP-H6VA1KR>
2023-11-17 12:50:57 +0530

    TC1 1st commit

---
+++ advanceVtiger/src/test/java/advanceVtiger/Testcase1.java
@@ -0,0 +1,17 @@
+package advanceVtiger;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testcase1 {
+	
+	@Test
+	public void t1()
+	{
+		Reporter.log("t1 running",true);
+		
+		
+		
+	}
+
+}
