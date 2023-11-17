b0e6c81a33d12e1f76cc20f02b789251979a3182
Admin <Admin@DESKTOP-H6VA1KR>
2023-11-17 13:08:28 +0530

    eng1 2nd commit

---
+++ advanceVtiger/src/test/java/advanceVtiger/Testcase2.java
@@ -0,0 +1,18 @@
+package advanceVtiger;
+
+import org.testng.annotations.Test;
+
+public class Testcase2 {
+	
+	@Test
+	public void t2()
+	{
+		 System.out.println("t2 is working");
+	}
+	
+	
+	
+	
+	
+
+}
